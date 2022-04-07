f0491ec608b6656ac724cfecdd7773f92005a29a
Gauss15 <95584122+Gauss15@users.noreply.github.com>
2022-04-07 13:46:53 -0500

    Add files via upload

---
+++ Week1/Extra Practice/First Java Program/First.java
@@ -0,0 +1,7 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("My name is Josh");
+        System.out.println("I am 26 years old");
+        System.out.println("My hometown is Sioux Falls, SD");
+    }
+}

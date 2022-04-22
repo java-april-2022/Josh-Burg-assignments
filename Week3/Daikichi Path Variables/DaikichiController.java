31b30e02cd3beb4a84a2a7af1d2b727ce6e1f260
Gauss15 <95584122+Gauss15@users.noreply.github.com>
2022-04-22 15:04:22 -0500

    Add files via upload

---
+++ Week3/Daikichi Path Variables/DaikichiController.java
@@ -0,0 +1,43 @@
+package com.joshburg.daikichi;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/daikichi")
+
+public class DaikichiController {
+	@RequestMapping("")
+	public String daikichi() {
+		return "Welcome!";
+	}
+	
+	@RequestMapping("/today")
+	public String today() {
+		return "Today you will find luck in all your endeavors!";		
+	}
+	
+	@RequestMapping("/tomorrow")
+	public String tomorrow() {
+		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+    @RequestMapping("/travel/{city}")
+    public String travelCity(@PathVariable("city") String city){
+    	return "Congradulation! you will be traveling to " + city + "!";
+    }
+    
+    @RequestMapping("/lotto/{int}")
+    public String daikichiLotto(@PathVariable("int") int Integer){
+    	if (Integer % 2 == 0) {
+    	return "You will take a grand journey in the near future, but be weary of tempting offers.";
+    	}
+    	else {
+    		return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+    	}
+    	
+    	
+    }
+    
+}

382d1f687b4080dd068bb67b4348b20cb4a4f1df
Gauss15 <95584122+Gauss15@users.noreply.github.com>
2022-04-23 19:14:58 -0500

    Add files via upload

---
+++ Week3/Fruity Loops/FruitLoops/src/test/java/com/joshburg/fruitloops/FruitLoopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshburg.fruitloops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitLoopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
